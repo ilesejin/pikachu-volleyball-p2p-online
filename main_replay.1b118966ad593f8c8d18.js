58b7e5cd23947016736eb6c67dbf26af0e782359
ilesejin <ilesejin@naver.com>
2024-12-31 20:53:43 +0900

    Updates

---
+++ main_replay.1b118966ad593f8c8d18.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpikachu_volleyball_p2p_online=self.webpackChunkpikachu_volleyball_p2p_online||[]).push([[640],{2251:(l,n,S)=>{var e=S(5093),p=S(6229);!function(){e.d.SPRITE_SHEET="../"+e.d.SPRITE_SHEET;for(const l in e.d.SOUNDS)e.d.SOUNDS[l]="../"+e.d.SOUNDS[l]}(),(0,p.yA)()}},l=>{l.O(0,[809,341,494],(()=>(2251,l(l.s=2251)))),l.O()}]);
+//# sourceMappingURL=main_replay.1b118966ad593f8c8d18.js.map
